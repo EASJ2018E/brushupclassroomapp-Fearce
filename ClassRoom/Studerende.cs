4e94811473864c1bbaa818f0016ccf8ba192e083
Fearce <thekev93@gmail.com>
2018-09-05 13:37:17 +0200

    opgave 5

--- ClassRoom/Studerende.cs
+++ ClassRoom/Studerende.cs
@@ -19,6 +19,31 @@ namespace ClassRoom
             Fødselsdagsår = fødselsdagsår;
         }
 
+        public string Årstid()
+        {
+            switch (Fødselsdagsmåned)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Vinter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Forår";
+                case 6:
+                case 7:
+                case 8:
+                    return "Sommer";
+                case 9:
+                case 10:
+                case 11:
+                    return "Efterår";
+                default:
+                    return "Fejl i måned";
+            }
+        }   
+
         public override string ToString()
         {
             return $"{nameof(Navn)}: {Navn}, {nameof(Fødselsdagsmåned)}: {Fødselsdagsmåned}, {nameof(Fødselsdagsår)}: {Fødselsdagsår}";
