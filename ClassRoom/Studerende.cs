599e423a69541150f60c50542d1b38693696e0d6
Fearce <thekev93@gmail.com>
2018-09-05 13:16:59 +0200

    Initial commit

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        public string Navn { get; private set; }
+        public int Fødselsdagsmåned { get; set; }   
+
+    }
+}
