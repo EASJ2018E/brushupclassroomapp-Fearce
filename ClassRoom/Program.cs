fded822145c06c8109f9fe3bc39ed3e1bf49f3c3
Fearce <thekev93@gmail.com>
2018-09-05 13:27:46 +0200

    opgave 3

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,14 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+
+              KlasseRum klasse = new KlasseRum();
+            klasse.KlasseNavn = "3B";
+            klasse.SemesterStart = DateTime.Parse("04-09-2018");
+            klasse.KlasseListe.Add(new Studerende("Kevin", 11, 93));
+            klasse.KlasseListe.Add(new Studerende("Michael", 1, 91));
+
+
         }
     }
 }
